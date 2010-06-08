2d885357bb37bf65d045af3f988588a73b8e23ed
James Michael DuPont <mdupont@introspector-mail.(none)>
2010-06-08 09:33:35 +0200

    first generated java code that implements mapnik filter expressions, also a skeleton class to eval them

---
+++ styles/generatecode.sh
@@ -0,0 +1,1 @@
+perl parsestyles.pl osm_expanded.xml > ../src/org/openstreetmap/gwt/client/GeneratedStyleEvaluator.java
--- styles/parsestyles.pl
+++ styles/parsestyles.pl
@@ -178,7 +178,7 @@ sub start
     #print Dumper($data);    
     return $data;
 }
-
+my $count =0;
 sub end
 {
     my $class=shift;
@@ -189,9 +189,14 @@ sub end
     if ($x)
     {
 	#print "good text $text \n";
-	my $xml  = $x->emitxml();
-	
-	print $xml;
+	my $java  = $x->emitJava();
+
+print "boolean ProcessStyle$count (OsmPrimitiveWrapper obj)
+	{\n";
+	print "return $java;\n";
+print "}; \n";
+	$count++;
+
     }
     else
     {
@@ -200,6 +205,15 @@ sub end
 
 }
 
+sub EmitCalls
+{
+print "void ProcessStyle (OsmPrimitiveWrapper obj){";
+foreach (0..$count -1)
+{
+    print "if( ProcessStyle$_ (obj)) {return; }\n";
+}
+print "}";
+}
 sub characters 
 {
     my $class=shift;
@@ -228,12 +242,11 @@ base class for all parser objects
 package Expression;
 use Moose;
 use Data::Dumper;
-sub emitxml
+sub emitJava
 {
-#    my $class=shift;
     my $self=shift;
-    # now this 
-#    warn Dumper($self);
+    my $s="";
+    my @a;
     foreach my $k (keys %{$self})
     {
 	my $v =$self->{$k};
@@ -247,8 +260,20 @@ sub emitxml
 #	print "$k has $v\n" if $k eq "__VALUE__";
 	next if $k eq "__VALUE__";
 #	print "$k \t $v\n";
-	$self->{$k}->emitxml();
+	$v = $self->{$k}->emitJava();
+	
+	if ($v)
+	{
+	    push @a,$v;
+	}
+	else
+	{
+	    warn Dumper($self->{$k});	    
+	}
     }
+
+    $s = "Concat(". join (",",@a) . ")";
+    return "Or($s)";
 }
 
 1;
@@ -256,69 +281,282 @@ sub emitxml
 package ExprNot;
 use Moose;
 extends 'Expression';
+
+sub emitJava
+{
+#    my $class=shift;
+    my $self=shift;
+    my $s="";
+    # now this 
+
+    foreach my $k (keys %{$self})
+    {
+	my $v =$self->{$k};
+       
+	next if $k eq "__RULE__";
+	next if $k eq "__STRING__";
+	next if $k eq "__STRING1__";
+	next if $k eq "__STRING2__";
+
+	$v = $self->{$k}->emitJava();
+	
+	if ($v)
+	{
+	    $s .= $v;
+	}
+	else
+	{
+	    warn Dumper($self->{$k});	    
+	}
+    }
+    return "Not($s)";
+}
+
 1;
 
 package Literal;
 use Moose;
 extends 'Expression';
+
+sub emitJava
+{
+#    my $class=shift;
+    my $self=shift;
+    my $id = $self->{__VALUE__};
+    return "\"$id\"";
+}
+
 1;
 
+=pod
+the basic field expression,
+field op value 
+=cut
 package ExprField;
 use Moose;
 extends 'Expression';
-1;
 
-package Field;
-use Moose;
-extends 'Expression';
+sub emitJava
+{
+    my $self=shift;
+    if ( $self->{Value})
+    {
+	my $field = $self->{Field}->emitJava();
+	my $op = $self->{Operator}->emitJava();
+	my $val = $self->{Value}->emitJava();
+	return "Condition(${field}.${op}(${val}))";
+    }
+    else
+    {
+	return $self->{Field}->emitJava();
+    }
+}
+
+
 1;
 
 package Identifier;
 use Moose;
 extends 'Expression';
+
+sub emitJava
+{
+    my $self=shift;
+    return $self->{__VALUE__};
+}
+
 1;
+
+
+package Field;
+use Moose;
+use Data::Dumper;
+extends 'Expression';
+
+sub emitJava
+{
+#    my $class=shift;
+    my $self=shift;
+    my $id = $self->{Identifier}->emitJava();
+    return "obj.get(\"$id\")";
+}
+
+1;
+
 
 package Operator;
 use Moose;
 extends 'Expression';
+sub emitJava
+{
+    my $self=shift;
+    my $id = $self->{__VALUE__} || "TODO";
+
+    if ($id eq "=")
+    {
+	return "equals"
+    }
+    elsif ($id eq "<>")
+    {
+	return "notequals"
+    }
+
+    return " OPERATOR $id ";
+}
+
 1;
 
 package Digits;
 use Moose;
 extends 'Expression';
+sub emitJava
+{
+    my $self=shift;
+    # foreach my $k (keys %{$self})
+    # {
+    # 	print "DIGITS $k \t $self->{$k}\n";
+    # }
+    my $id = $self->{__VALUE__} || "TODO";
+    return "\"$id\"";
+}
+
 1;
 
 package Value;
 use Moose;
 extends 'Expression';
+use Data::Dumper;
+sub emitJava
+{
+    my $self=shift;
+    if ($self->{"Literal"})
+    {
+	my $id = $self->{"Literal"}->emitJava();
+	return "$id";
+    }    
+    elsif ($self->{"Digits"})
+    {
+	my $id = $self->{"Digits"}->emitJava();
+	return "$id";
+    }
+    else
+    {
+	my $v = $self->{"__VALUE__"};
+
+	if ($v)
+	{
+	    if ($v eq "''")
+	    {
+		return "\"\"";
+	    }
+	    return "$v";
+	}
+	else
+	{
+	    warn "What is this value" . Dumper($self);
+	    die "no data";
+	}
+
+# 	foreach my $k (keys %{$self})
+# 	{
+# 	    next if $k eq "__RULE__";
+# 	    print "VALUE:$k \t $self->{$k}\n";
+# #	    warn Dumper($self->{$k});
+# 	}
+    }
+}
+
 1;
 
 package Parens;
 use Moose;
 extends 'Expression';
+
+
+sub emitJava
+{
+#    my $class=shift;
+    my $self=shift;
+    my $s="";
+    # now this 
+
+    foreach my $k (keys %{$self})
+    {
+	my $v =$self->{$k};
+       
+	next if $k eq "__RULE__";
+	next if $k eq "__STRING__";
+	next if $k eq "__STRING1__";
+	next if $k eq "__STRING2__";
+
+	$v = $self->{$k}->emitJava();
+	
+	if ($v)
+	{
+	    $s .= $v;
+	}
+	else
+	{
+	    warn Dumper($self->{$k});	    
+	}
+    }
+    return "Parens($s)";
+}
 1;
 
 package ExprAnd;
 use Moose;
 extends 'Expression';
+
+sub emitJava
+{
+
+    my $self=shift;
+    my $s="";
+    my @a;
+
+    foreach my $k (keys %{$self})
+    {
+	my $v =$self->{$k};
+       
+	next if $k eq "__RULE__";
+	next if $k eq "__STRING__";
+	next if $k eq "__STRING1__";
+	next if $k eq "__STRING2__";
+
+	$v = $self->{$k}->emitJava();
+
+	push @a, $v;	
+
+    }
+    $s = "Concat(". join (",",@a) . ")";
+    return "And($s)";
+}
+
 1;
 
 package FilterExpr;
 
 use Data::Dumper;
 
-sub emitxml
+sub emitJava
 {
     my $self=shift;
-    # now this 
-    #warn Dumper($self);
+    my $s="";
+    my @a;
+
     foreach my $k (keys %{$self})
     {
 	next if $k eq "__RULE__";
 	#print "$k \t $self->{$k}\n";
-	$self->{$k}->emitxml();
+	
+	push @a, $self->{$k}->emitJava();
 
     }
+    $s = "Concat(". join (",",@a) . ")";
+
+
+    return "Filter($s)";
 }
 
 
@@ -383,4 +621,14 @@ use warnings;
 use Data::Dumper;
 use XML::SAX;
 my $parser = XML::SAX::ParserFactory->parser(    Handler => MySAXHandler->new    );
+
+print "package org.openstreetmap.gwt.client;\n";
+print "import  org.openstreetmap.gwt.client.StyleEvaluator;\n";
+print "import org.openstreetmap.josm.data.osm.OsmPrimitiveWrapper;\n";
+print "public class GeneratedStyleEvaluator extends StyleEvaluator  { \n";
+
 $parser->parse_uri("osm_expanded.xml");
+
+Filter::EmitCalls();
+
+print "}; \n";
