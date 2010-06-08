62e9767de83d0e8121bc191ecf8f39fe2c1734c1
James Michael DuPont <mdupont@introspector-mail.(none)>
2010-06-08 07:06:22 +0200

    added in a class hierarchy for the parser rules

--- styles/parsestyles.pl
+++ styles/parsestyles.pl
@@ -1,401 +1,175 @@
 #!/usr/bin/perl
-
+# author : james michael dupont
+# license : aGPL 3.0
 use strict;
 use warnings;
 
+=pod
+base class for all sax callback rules
+=cut
 
-package Rule;
-use Data::Dumper;
+package BaseRule;
+use Moose;
 sub start
 {
-    my $self=shift;
-    #print Dumper($self);    
 }
-sub characters 
-{
-
-}
-
 sub end
 {
-
 }
+
+sub characters 
+{
+}
+
+1;
+
+
+package Rule;
+use Moose;
+extends 'BaseRule';
 
 1;
 
 package MaxScaleDenominator;
-
-sub start
-{
-
-}
-sub end
-{
-
-}
-
-sub characters 
-{
-
-}
+use Moose;
+extends 'BaseRule';
 
 1;
 
 package MinScaleDenominator;
-
-sub start
-{
-
-}
-sub end
-{
-
-}
-
-sub characters 
-{
-
-}
-
+use Moose;
+extends 'BaseRule';
 1;
 
 package PointSymbolizer;
+use Moose;
+extends 'BaseRule';
 
-sub start
-{
-
-}
-sub end
-{
-
-}
-
-sub characters 
-{
-
-}
 
 1;
 
 package Map;
-use Data::Dumper;
-sub start
-{
-
-}
-sub end
-{
-
-}
-sub characters 
-{
-
-}
+use Moose;
+extends 'BaseRule';
 
 1;
 
 package FontSet;
-use Data::Dumper;
-sub start
-{
-
-}
-sub end
-{
-
-}
-sub characters 
-{
-
-}
+use Moose;
+extends 'BaseRule';
 
 1;
 
 package Font;
-use Data::Dumper;
-sub start
-{
-
-}
-sub end
-{
-
-}
-sub characters 
-{
-
-}
+use Moose;
+extends 'BaseRule';
 
 1;
 
 package Style;
-use Data::Dumper;
-sub start
-{
-
-}
-sub end
-{
-
-}
-sub characters 
-{
-
-}
+use Moose;
+extends 'BaseRule';
 
 1;
 
 package LineSymbolizer;
-use Data::Dumper;
-sub start
-{
-
-}
-sub end
-{
-
-}
-sub characters 
-{
-
-}
+use Moose;
+extends 'BaseRule';
 
 1;
 
 package PolygonSymbolizer;
-use Data::Dumper;
-sub start
-{
-
-}
-sub end
-{
-
-}
-sub characters 
-{
-
-}
+use Moose;
+extends 'BaseRule';
 
 1;
 
 package PolygonPatternSymbolizer;
-use Data::Dumper;
-sub start
-{
+use Moose;
+extends 'BaseRule';
 
-}
-sub end
-{
-
-}
-sub characters 
-{
-
-}
 
 1;
 
 package TextSymbolizer;
-use Data::Dumper;
-sub start
-{
+use Moose;
+extends 'BaseRule';
 
-}
-sub end
-{
-
-}
-sub characters 
-{
-
-}
 
 1;
 
 package ElseFilter;
-use Data::Dumper;
-sub start
-{
-
-}
-sub end
-{
-
-}
-sub characters 
-{
-
-}
+use Moose;
+extends 'BaseRule';
 
 1;
 
 package LinePatternSymbolizer;
-use Data::Dumper;
-sub start
-{
-
-}
-sub end
-{
-
-}
-sub characters 
-{
-
-}
+use Moose;
+extends 'BaseRule';
 1;
 
 package Layer;
-use Data::Dumper;
-sub start
-{
-
-}
-sub end
-{
-
-}
-sub characters 
-{
-
-}
+use Moose;
+extends 'BaseRule';
 1;
 
 package StyleName;
-use Data::Dumper;
-sub start
-{
+use Moose;
+extends 'BaseRule';
 
-}
-sub end
-{
-
-}
-sub characters 
-{
-
-}
 1;
 
 package Datasource;
-use Data::Dumper;
-sub start
-{
+use Moose;
+extends 'BaseRule';
 
-}
-sub end
-{
-
-}
-sub characters 
-{
-
-}
 1;
 
 package Parameter;
-use Data::Dumper;
-sub start
-{
+use Moose;
+extends 'BaseRule';
 
-}
-sub end
-{
-
-}
-sub characters 
-{
-
-}
 1;
 
 
 package ShieldSymbolizer;
-use Data::Dumper;
-sub start
-{
+use Moose;
+extends 'BaseRule';
 
-}
-sub end
-{
-
-}
-sub characters 
-{
-
-}
 
 1;
 
 package CssParameter;
-use Data::Dumper;
-sub start
-{
+use Moose;
+extends 'BaseRule';
 
-}
-sub end
-{
-
-}
-sub characters 
-{
-
-}
 
 1;
 
 package Filter;
 use Parse::RecDescent;
+use Data::Dumper;
 
-
-# ( ) Enclose a sub-expression
-# [ ] Enclose a Field/Attribute Name
-# * multiply
-# / divide
-# + add
-# - subtract
-# % modulo
-# = equal to or equivalent to
-# &lt;&gt;
-# !=
-# <> not equal
-# not equal
-# &lt; < less than
-# &lt;= <= less than or equal
-# &gt; > greater than
-# &gt;= >= greater than or equal
-# and and
-# or and
 
 my $grammar = q {
 <autotree>
 
-startrule : expression 
-expression  :  and_expr 'or' expression | and_expr
-and_expr : not_expr 'and' and_expr | not_expr
-not_expr : 'not' parens | '<>' parens | parens
-parens : '(' expression ')' | sexpression
-sexpression : field operator value   | field 
-identifier: /[A-Za-z_][A-Za-z0-9_]*/
-videntifier: /[A-Za-z0-9_]+/
-field : '[' identifier ']'
-digits : /[0-9]+/
-value : "\'" videntifier "\'" | digits | "\'\'"
-operator :  '*' |  '/' | '+' | '-' | '%' | '=' | '<>'
-
+FilterExpr : Expression 
+Expression  :  ExprAnd 'or' Expression | ExprAnd
+ExprAnd : ExprNot 'and' ExprAnd | ExprNot
+ExprNot : 'not' Parens | '<>' Parens | Parens
+Parens : '(' Expression ')' | ExprField
+ExprField : Field Operator Value   | Field 
+Identifier: /[A-Za-z_][A-Za-z0-9_]*/
+Literal: /[A-Za-z0-9_]+/
+Field : '[' Identifier ']'
+Digits : /[0-9]+/
+Value : "\'" Literal "\'" | Digits | "\'\'"
+Operator :  '*' |  '/' | '+' | '-' | '%' | '=' | '<>'
          };
-
-
-#   parens and not expression
-#([waterway]='drain' or [waterway]='ditch') and not [tunnel]='yes'
-
-#parens | sexpression  # | sexpression 'and' expression  | 'not ' expression |  sexpression 'or' expression
-#| 'or' | 'and' | 'and not' | 'not'  | '<>' | '!='
 my $ruleparser = new Parse::RecDescent ($grammar) or die "Bad grammar!\n";
 
-
-use Data::Dumper;
 sub start
 {
     my $class=shift;
@@ -411,16 +185,18 @@ sub end
     my $self=shift;
 #    warn Dumper($self);
     my $text =$self->{chars};
-    my $x= $ruleparser->startrule($text) ;
+    my $x= $ruleparser->FilterExpr($text) ;
     if ($x)
     {
-#	print "good text $text \n";
+	#print "good text $text \n";
+	my $xml  = $x->emitxml();
+	
+	print $xml;
     }
     else
     {
 	print "Bad text! $text\n";
     }
-
 
 }
 
@@ -439,6 +215,109 @@ sub characters
     else
     {
 	$self->{chars} = $string;
+    }
+}
+
+
+1;
+
+=pod
+base class for all parser objects
+=cut
+
+package Expression;
+use Moose;
+use Data::Dumper;
+sub emitxml
+{
+#    my $class=shift;
+    my $self=shift;
+    # now this 
+#    warn Dumper($self);
+    foreach my $k (keys %{$self})
+    {
+	my $v =$self->{$k};
+       
+	next if $k eq "__RULE__";
+	next if $k eq "__STRING__";
+	next if $k eq "__STRING1__";
+	next if $k eq "__STRING2__";
+
+
+#	print "$k has $v\n" if $k eq "__VALUE__";
+	next if $k eq "__VALUE__";
+#	print "$k \t $v\n";
+	$self->{$k}->emitxml();
+    }
+}
+
+1;
+
+package ExprNot;
+use Moose;
+extends 'Expression';
+1;
+
+package Literal;
+use Moose;
+extends 'Expression';
+1;
+
+package ExprField;
+use Moose;
+extends 'Expression';
+1;
+
+package Field;
+use Moose;
+extends 'Expression';
+1;
+
+package Identifier;
+use Moose;
+extends 'Expression';
+1;
+
+package Operator;
+use Moose;
+extends 'Expression';
+1;
+
+package Digits;
+use Moose;
+extends 'Expression';
+1;
+
+package Value;
+use Moose;
+extends 'Expression';
+1;
+
+package Parens;
+use Moose;
+extends 'Expression';
+1;
+
+package ExprAnd;
+use Moose;
+extends 'Expression';
+1;
+
+package FilterExpr;
+
+use Data::Dumper;
+
+sub emitxml
+{
+    my $self=shift;
+    # now this 
+    #warn Dumper($self);
+    foreach my $k (keys %{$self})
+    {
+	next if $k eq "__RULE__";
+	#print "$k \t $self->{$k}\n";
+	$self->{$k}->emitxml();
+
     }
 }
 
@@ -468,29 +347,6 @@ use base qw(XML::SAX::Base);
 use Data::Dumper;
 my %seen;
 
-## elements
-# Map
-# FontSet
-# Font
-# Style
-# Rule
-# MaxScaleDenominator
-# MinScaleDenominator
-# PointSymbolizer
-# Filter
-# LineSymbolizer
-# CssParameter
-# PolygonSymbolizer
-# PolygonPatternSymbolizer
-# TextSymbolizer
-# ElseFilter
-# ShieldSymbolizer
-# LinePatternSymbolizer
-# Layer
-# StyleName
-# Datasource
-# Parameter
-
 
 my $current="Nothing";
 my $obj = undef;
@@ -498,17 +354,7 @@ sub start_element
 {
     my ($self, $el) = @_;
     my $name = $el->{LocalName};
-    
-    #print "$name\n" unless $seen{$name}++;    
-    # if ($name eq "Rule")
-    # {
-    # 	Rule::start($el);
-    # }
-
-#    if ($name eq "Filter")
-#    {
     $obj = ${name}->start($el);
-#    }
     $current = $name;
 }
 
